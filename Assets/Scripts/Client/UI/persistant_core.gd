0cec8ad57f732b74095096de94660f2059d4e4e2
Duskitty <Theduskitty@gmail.com>
2024-12-31 12:12:34 -0500

    Color Picker Re-incorperated

--- Assets/Scripts/Client/UI/persistant_core.gd
+++ Assets/Scripts/Client/UI/persistant_core.gd
@@ -315,8 +315,8 @@ func _on_part_button_pressed(PartData: String) -> void:
 		$CanvasLayer/Hexii_Tablet_UI/Wallpaper/Character_Screen/Color_Picker.show()
 		current_colorselected = Asset
 		
-		var color:Color = Hexii_Ui_Tablet_Character_OBJ.get(Asset)
-		var colorEmiss:Color = Hexii_Ui_Tablet_Character_OBJ.get(Asset+"_Emiss")
+		var color:Color = Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Color[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][Asset]]
+		var colorEmiss:Color = Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Emission[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][Asset]]
 		
 		print(colorEmiss)
 		
@@ -326,9 +326,9 @@ func _on_part_button_pressed(PartData: String) -> void:
 		Hexii_Ui_Tablet_Color_Container.get_node("Hex_Input/LineEdit").text = str(color.to_html(false))
 		Hexii_Ui_Tablet_Color_Container.get_node("Emission_Input/LineEdit").text = str(colorEmiss.to_html(false))
 
-		Hexii_Ui_Tablet_Color_Container.get_node("E_S/LineEdit").text = str(Hexii_Ui_Tablet_Character_OBJ.get(Asset+"_Emiss_S"))
-		Hexii_Ui_Tablet_Color_Container.get_node("RO/LineEdit").text = str(Hexii_Ui_Tablet_Character_OBJ.get(Asset+"_Roughness"))
-		Hexii_Ui_Tablet_Color_Container.get_node("M/LineEdit").text = str(Hexii_Ui_Tablet_Character_OBJ.get(Asset+"_Metallic"))
+		Hexii_Ui_Tablet_Color_Container.get_node("E_S/LineEdit").text = str( Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Emission_Strength[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][Asset]])
+		Hexii_Ui_Tablet_Color_Container.get_node("RO/LineEdit").text = str( Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Roughness[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][Asset]])
+		Hexii_Ui_Tablet_Color_Container.get_node("M/LineEdit").text = str( Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Metallic[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][Asset]])
 		initialpress = true
 		_on_color_text_change("", "Hex")
 		initialpress = false
@@ -426,7 +426,7 @@ func _on_color_text_change(new_text:String, type:String) -> void:
 			Hexii_Ui_Tablet_Color_Container.get_node("R/LineEdit").text = str(color.r8)
 			Hexii_Ui_Tablet_Color_Container.get_node("G/LineEdit").text = str(color.g8)
 			Hexii_Ui_Tablet_Color_Container.get_node("B/LineEdit").text = str(color.b8)
-			Hexii_Ui_Tablet_Character_OBJ.set(current_colorselected,Color8(color.r8,color.g8,color.b8))
+			Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Color[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][current_colorselected]] = Color8(color.r8,color.g8,color.b8)
 		
 			
 		if Hexii_Ui_Tablet_Color_Container.get_node("Emission_Input/LineEdit").text.is_valid_html_color():
@@ -437,7 +437,7 @@ func _on_color_text_change(new_text:String, type:String) -> void:
 			Hexii_Ui_Tablet_Color_Container.get_node("E_R/LineEdit").text = str(color.r8)
 			Hexii_Ui_Tablet_Color_Container.get_node("E_G/LineEdit").text = str(color.g8)
 			Hexii_Ui_Tablet_Color_Container.get_node("E_B/LineEdit").text = str(color.b8)
-			Hexii_Ui_Tablet_Character_OBJ.set(current_colorselected+"_Emiss",Color8(color.r8,color.g8,color.b8))
+			Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Emission[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][current_colorselected]] = Color8(color.r8,color.g8,color.b8)
 		
 	elif type == "RGB":
 		var color = Color(
@@ -445,12 +445,11 @@ func _on_color_text_change(new_text:String, type:String) -> void:
 			int(Hexii_Ui_Tablet_Color_Container.get_node("G/LineEdit").text),
 			int(Hexii_Ui_Tablet_Color_Container.get_node("B/LineEdit").text)
 		)
-		print(color.r)
 		Hexii_Ui_Tablet_Color_Container.get_node("Hex_Input/LineEdit").text = color.to_html(false)
 		Hexii_Ui_Tablet_Color_Container.get_node("R/HSlider").value = color.r
 		Hexii_Ui_Tablet_Color_Container.get_node("G/HSlider").value = color.g
 		Hexii_Ui_Tablet_Color_Container.get_node("B/HSlider").value = color.b
-		Hexii_Ui_Tablet_Character_OBJ.set(current_colorselected,Color8(int(color.r),int(color.g),int(color.b)))
+		Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Color[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][current_colorselected]] = Color8(int(color.r),int(color.g),int(color.b))
 		
 		var colorEmiss = Color(
 			int(Hexii_Ui_Tablet_Color_Container.get_node("E_R/LineEdit").text),
@@ -461,9 +460,9 @@ func _on_color_text_change(new_text:String, type:String) -> void:
 		Hexii_Ui_Tablet_Color_Container.get_node("E_R/HSlider").value = colorEmiss.r
 		Hexii_Ui_Tablet_Color_Container.get_node("E_G/HSlider").value = colorEmiss.g
 		Hexii_Ui_Tablet_Color_Container.get_node("E_B/HSlider").value = colorEmiss.b
-		Hexii_Ui_Tablet_Character_OBJ.set(current_colorselected+"_Emiss",Color8(int(colorEmiss.r),int(colorEmiss.g),int(colorEmiss.b)))
+		Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Emission[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][current_colorselected]] = Color8(int(colorEmiss.r),int(colorEmiss.g),int(colorEmiss.b))
 		
-	Hexii_Ui_Tablet_Character_OBJ.Regen_Color()
+	Hexii_Ui_Tablet_Character_OBJ.Hub.generate_colors()
 	update_values() 
 
 func _on_color_value_changed(value: float) -> void:
@@ -483,21 +482,21 @@ func _on_color_value_changed(value: float) -> void:
 		var BE = int(Hexii_Ui_Tablet_Color_Container.get_node("E_B/HSlider").value)
 		var SE = Hexii_Ui_Tablet_Color_Container.get_node("E_S/HSlider").value
 		
-		Hexii_Ui_Tablet_Character_OBJ.set(current_colorselected,Color8(R,G,B))
-		Hexii_Ui_Tablet_Character_OBJ.set(current_colorselected+"_Emiss",Color8(RE,GE,BE))
+		Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Color[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][current_colorselected]] = Color8(R,G,B)
+		Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Emission[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][current_colorselected]] = Color8(RE,GE,BE)
 		
-		Hexii_Ui_Tablet_Character_OBJ.set(current_colorselected+"_Roughness",Roughness)
-		Hexii_Ui_Tablet_Character_OBJ.set(current_colorselected+"_Metallic",Metallic)
-		Hexii_Ui_Tablet_Character_OBJ.set(current_colorselected+"_Emiss_S",SE)
+		Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Roughness[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][current_colorselected]] = Roughness
+		Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Metallic[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][current_colorselected]] = Metallic
+		Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Emission_Strength[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][current_colorselected]] = SE
 		
-		Hexii_Ui_Tablet_Character_OBJ.Regen_Color()
+		Hexii_Ui_Tablet_Character_OBJ.Hub.generate_colors()
 		update_values() 
 
 func update_values() -> void:
 	$CanvasLayer/Hexii_Tablet_UI/Wallpaper/Character_Screen/HBoxContainer/Button4.show()
 	$CanvasLayer/Hexii_Tablet_UI/Wallpaper/Character_Screen/HBoxContainer/Button3.show()
-	var color:Color = Hexii_Ui_Tablet_Character_OBJ.get(current_colorselected)
-	var colorEmiss:Color = Hexii_Ui_Tablet_Character_OBJ.get(current_colorselected+"_Emiss")
+	var color:Color = Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Color[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][current_colorselected]]
+	var colorEmiss:Color = Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Emission[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][current_colorselected]]
 	
 	Hexii_Ui_Tablet_Color_Container.get_node("Emiss_Color_Rect").color =  colorEmiss
 	Hexii_Ui_Tablet_Color_Container.get_node("Base_Color_Rect").color =  color
@@ -513,10 +512,10 @@ func update_values() -> void:
 	Hexii_Ui_Tablet_Color_Container.get_node("E_G/LineEdit").text = str(colorEmiss.g8)
 	Hexii_Ui_Tablet_Color_Container.get_node("E_B/LineEdit").text = str(colorEmiss.b8)
 	
-	Hexii_Ui_Tablet_Color_Container.get_node("E_S/LineEdit").text = str(Hexii_Ui_Tablet_Character_OBJ.get(current_colorselected+"_Emiss_S"))
+	Hexii_Ui_Tablet_Color_Container.get_node("E_S/LineEdit").text = str(Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Emission_Strength[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][current_colorselected]])
 	
-	Hexii_Ui_Tablet_Color_Container.get_node("RO/LineEdit").text = str(Hexii_Ui_Tablet_Character_OBJ.get(current_colorselected+"_Roughness"))
-	Hexii_Ui_Tablet_Color_Container.get_node("M/LineEdit").text = str(Hexii_Ui_Tablet_Character_OBJ.get(current_colorselected+"_Metallic"))
+	Hexii_Ui_Tablet_Color_Container.get_node("RO/LineEdit").text = str(Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Roughness[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][current_colorselected]])
+	Hexii_Ui_Tablet_Color_Container.get_node("M/LineEdit").text = str(Hexii_Ui_Tablet_Character_OBJ.Hub.Shader_Metallic[Hexii_Ui_Tablet_Character_OBJ.Hub.Keylist["Body"][current_colorselected]])
 
 func _on_load_button_pressed() -> void:
 	Core.AssetData.Tools.generate_character_from_string($CanvasLayer/Hexii_Tablet_UI/Wallpaper/Character_Screen/Code_Loader/ScrollContainer_Color/GridContainer2/Code_Input/LineEdit.text, Hexii_Ui_Tablet_Character_OBJ.Hub)
--- addons/Cubiix_Assets/Scripts/Character_Tools.gd
+++ addons/Cubiix_Assets/Scripts/Character_Tools.gd
@@ -248,11 +248,31 @@ func generate_character_from_string(Data:String,Target:Node3D) -> void:
 			Target.generate_character()
 
 func clone_character(A:Node3D, B:Node3D) -> void:
-	B.Shader_Color = A.Shader_Color.duplicate()
-	B.Shader_Emission = A.Shader_Emission.duplicate()
-	B.Shader_Emission_Strength = A.Shader_Emission_Strength.duplicate()
-	B.Shader_Roughness = A.Shader_Roughness.duplicate()
-	B.Shader_Metallic = A.Shader_Metallic.duplicate()
+	B.Shader_Color[B.Keylist["Body"]["Body1"]] = A.Shader_Color[B.Keylist["Body"]["Body1"]]
+	B.Shader_Color[B.Keylist["Body"]["Body2"]] = A.Shader_Color[B.Keylist["Body"]["Body2"]]
+	B.Shader_Color[B.Keylist["Body"]["Eye1"]] = A.Shader_Color[B.Keylist["Body"]["Eye1"]]
+	B.Shader_Color[B.Keylist["Body"]["Eye2"]] = A.Shader_Color[B.Keylist["Body"]["Eye2"]]
+	
+	B.Shader_Emission[B.Keylist["Body"]["Body1"]] = A.Shader_Emission[B.Keylist["Body"]["Body1"]]
+	B.Shader_Emission[B.Keylist["Body"]["Body2"]] = A.Shader_Emission[B.Keylist["Body"]["Body2"]]
+	B.Shader_Emission[B.Keylist["Body"]["Eye1"]] = A.Shader_Emission[B.Keylist["Body"]["Eye1"]]
+	B.Shader_Emission[B.Keylist["Body"]["Eye2"]] = A.Shader_Emission[B.Keylist["Body"]["Eye2"]]
+	
+	B.Shader_Emission_Strength[B.Keylist["Body"]["Body1"]] = A.Shader_Emission_Strength[B.Keylist["Body"]["Body1"]]
+	B.Shader_Emission_Strength[B.Keylist["Body"]["Body2"]] = A.Shader_Emission_Strength[B.Keylist["Body"]["Body2"]]
+	B.Shader_Emission_Strength[B.Keylist["Body"]["Eye1"]] = A.Shader_Emission_Strength[B.Keylist["Body"]["Eye1"]]
+	B.Shader_Emission_Strength[B.Keylist["Body"]["Eye2"]] = A.Shader_Emission_Strength[B.Keylist["Body"]["Eye2"]]
+	
+	B.Shader_Roughness[B.Keylist["Body"]["Body1"]] = A.Shader_Roughness[B.Keylist["Body"]["Body1"]]
+	B.Shader_Roughness[B.Keylist["Body"]["Body2"]] = A.Shader_Roughness[B.Keylist["Body"]["Body2"]]
+	B.Shader_Roughness[B.Keylist["Body"]["Eye1"]] = A.Shader_Roughness[B.Keylist["Body"]["Eye1"]]
+	B.Shader_Roughness[B.Keylist["Body"]["Eye2"]] = A.Shader_Roughness[B.Keylist["Body"]["Eye2"]]
+	
+	B.Shader_Metallic[B.Keylist["Body"]["Body1"]] = A.Shader_Metallic[B.Keylist["Body"]["Body1"]]
+	B.Shader_Metallic[B.Keylist["Body"]["Body2"]] = A.Shader_Metallic[B.Keylist["Body"]["Body2"]]
+	B.Shader_Metallic[B.Keylist["Body"]["Eye1"]] = A.Shader_Metallic[B.Keylist["Body"]["Eye1"]]
+	B.Shader_Metallic[B.Keylist["Body"]["Eye2"]] = A.Shader_Metallic[B.Keylist["Body"]["Eye2"]]
+	
 	B.Base_Tails = A.Base_Tails
 	B.Base_Wings = A.Base_Wings
 	B.Base_Ears = A.Base_Ears
