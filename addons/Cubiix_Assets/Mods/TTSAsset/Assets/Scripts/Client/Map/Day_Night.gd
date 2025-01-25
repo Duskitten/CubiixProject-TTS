eec245a0e1d388eba347e9ce02eb55551032cd9b
Duskitty <Theduskitty@gmail.com>
2025-01-24 20:39:04 -0500

    No volumetrics, shader sky system

--- addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/Map/Day_Night.gd
+++ addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/Map/Day_Night.gd
@@ -5,6 +5,10 @@ var CurrentTick :int = 0
 var Tick_Prev = 0
 var Tick_Timer = 0
 var Current_Tick = 0
+var SkyMaterial:ShaderMaterial
+var TimeArray = [Color("#FFFFFF"),1.0, 1.0,Color("#059dff")]
+func _ready() -> void:
+	SkyMaterial = $"../WorldEnvironment".environment.sky.sky_material
 
 func _process(delta: float) -> void:
 	var Delta = Time.get_ticks_msec() - Tick_Prev
@@ -14,20 +18,26 @@ func _process(delta: float) -> void:
 	if Tick_Timer > 1000/20:
 		print(time_of_day)
 		Current_Tick += 1
-		time_of_day += .02
+		time_of_day += 0.01
 		if time_of_day >= 180:
 			time_of_day = -180
 		Tick_Timer = 0
 	
-	if time_of_day >= -180 && time_of_day <= -90:
-		print("A")
-		$"../WorldEnvironment".environment.ambient_light_color = lerp($"../WorldEnvironment".environment.ambient_light_color,Color("#6e9fd9"), 0.05)
-		$DirectionalLight3D.light_energy = lerp($DirectionalLight3D.light_energy,remap(time_of_day,-180,-90,0,2), 0.1)
-	if time_of_day >= -90 && time_of_day <= 0:
-		$"../WorldEnvironment".environment.ambient_light_color = lerp($"../WorldEnvironment".environment.ambient_light_color,Color("#6e9fd9"), 0.05)
-		$DirectionalLight3D.light_energy = lerp($DirectionalLight3D.light_energy,remap(time_of_day,-90,0,2,0), 0.1)
+	if time_of_day >= -180 && time_of_day <= 0:
+		TimeArray = [Color("#FFFFFF"),1.0, 1.0,Color("#059dff")]
+		#SkyMaterial.set_shader_parameter("SkyDarkness",lerp(float(SkyMaterial.get_shader_parameter("SkyDarkness")),1.0,0.002))
+	#	$"../WorldEnvironment".environment.ambient_light_color = lerp($"../WorldEnvironment".environment.ambient_light_color,Color("#6e9fd9"), 0.05)
+		#$DirectionalLight3D.light_energy = lerp($DirectionalLight3D.light_energy,1.0, .002)
+		#$DirectionalLight3D.light_color = lerp($DirectionalLight3D.light_color,Color("#ffffff"), 0.002)
+	#elif time_of_day >= -90 && time_of_day <= 0:
+		#SkyMaterial.set_shader_parameter("SkyDarkness",lerp(float(SkyMaterial.get_shader_parameter("SkyDarkness")),1.0,0.1))
+	#	$"../WorldEnvironment".environment.ambient_light_color = lerp($"../WorldEnvironment".environment.ambient_light_color,Color("#6e9fd9"), 0.05)
+		#$DirectionalLight3D.light_energy = lerp($DirectionalLight3D.light_energy,remap(time_of_day,-90,0,2,0), 0.1)
 	elif time_of_day > 0 && time_of_day < 180:
-		$"../WorldEnvironment".environment.ambient_light_color = lerp($"../WorldEnvironment".environment.ambient_light_color,Color("#3d3d69"), 0.05)
-		$DirectionalLight3D.light_energy = lerp($DirectionalLight3D.light_energy,remap(time_of_day,0,180,0,0), 0.1)
-	
-	self.rotation.x = lerp_angle(self.rotation.x,deg_to_rad(time_of_day),0.1)
+		TimeArray = [Color("#00135a"),0.0, 0.8,Color("#2f0061")]
+	SkyMaterial.set_shader_parameter("SkyDarkness",lerp(float(SkyMaterial.get_shader_parameter("SkyDarkness")),TimeArray[1],0.002))
+	#	$"../WorldEnvironment".environment.ambient_light_color = lerp($"../WorldEnvironment".environment.ambient_light_color,Color("#3d3d69"), 0.05)
+	$DirectionalLight3D.light_energy = lerp($DirectionalLight3D.light_energy,TimeArray[2], 0.002)
+	$DirectionalLight3D.light_color = lerp($DirectionalLight3D.light_color,TimeArray[0], 0.002)
+	$"../WorldEnvironment".environment.fog_light_color = lerp($"../WorldEnvironment".environment.fog_light_color,TimeArray[3], 0.002)
+	#self.rotation.x = lerp_angle(self.rotation.x,deg_to_rad(time_of_day),0.1)
