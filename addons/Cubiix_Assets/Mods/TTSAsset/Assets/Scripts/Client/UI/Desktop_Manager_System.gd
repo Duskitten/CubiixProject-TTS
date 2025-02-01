3d3ddf83882e51c81186646a71d9c9a0de4b39b6
Duskitty <Theduskitty@gmail.com>
2025-01-31 22:32:30 -0500

    Added Dotmouse's Solarpanels, More UI Progress

--- addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/UI/Desktop_Manager_System.gd
+++ addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/UI/Desktop_Manager_System.gd
@@ -14,6 +14,7 @@ func _on_transition_button_pressed(extra_arg_0: String) -> void:
 			else:
 				if lastmenu == extra_arg_0:
 					newnode.set_meta("Open",true)
+					newnode.show()
 					var tween = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_parallel(true).set_trans(Tween.TRANS_QUAD)
 					tween.tween_property(newnode, "scale", Vector2(1,1), .2)
 					tween.tween_property(newnode, "position", Vector2.ZERO, .2)
@@ -21,10 +22,12 @@ func _on_transition_button_pressed(extra_arg_0: String) -> void:
 	if lastmenu != extra_arg_0:
 		var newnode = get_node_or_null(extra_arg_0)
 		if newnode != null:
+			newnode.show()
 			newnode.set_meta("Open",true)
 			var tween = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_parallel(true).set_trans(Tween.TRANS_QUAD)
 			tween.tween_property(newnode, "scale", Vector2(1,1), .2)
 			tween.tween_property(newnode, "position", Vector2.ZERO, .2)
+			
 		
 	
 	lastmenu = extra_arg_0
--- addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/UI/title_settings_adjust.gd
+++ addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/UI/title_settings_adjust.gd
@@ -2,7 +2,8 @@ extends Control
 @onready var Core = get_node("/root/Main_Scene/CoreLoader")
 
 func _on_h_slider_value_changed(value: float,extra_arg_0: StringName, extra_arg_1:StringName) -> void:
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(extra_arg_0),value)
+	if extra_arg_1 == &"Audio":
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index(extra_arg_0),int(value))
 	Core.Globals.Data[str(extra_arg_1)][str(extra_arg_0)] = value
 
 func _ready() -> void:
@@ -10,7 +11,6 @@ func _ready() -> void:
 		var core = i.name
 		for x in i.get_children():
 			if x is BoxContainer:
-				print(Core.Globals.Data[str(core)])
 				x.get_node("HBoxContainer/HSlider").value = Core.Globals.Data[str(core)][str(x.name)]
 
 func hide_all() -> void:
@@ -19,4 +19,13 @@ func hide_all() -> void:
 
 func _on_button_pressed(extra_arg_0:String) -> void:
 	hide_all()
+	match extra_arg_0:
+		"Audio":
+			get_node("Audio/SFX/HBoxContainer/HSlider").value = Core.Globals.Data[str(extra_arg_0)]["SFX"]
+			get_node("Audio/Master/HBoxContainer/HSlider").value = Core.Globals.Data[str(extra_arg_0)]["Master"]
+			get_node("Audio/Music/HBoxContainer/HSlider").value = Core.Globals.Data[str(extra_arg_0)]["Music"]
+			get_node("Audio/Notification/HBoxContainer/HSlider").value = Core.Globals.Data[str(extra_arg_0)]["Notification"]
+			get_node("Audio/Ping/HBoxContainer/HSlider").value = Core.Globals.Data[str(extra_arg_0)]["Ping"]
+		"Visuals":
+			get_node("Visuals/Shadow_Depth/HBoxContainer/HSlider").value = Core.Globals.Data[str(extra_arg_0)]["Shadow_Depth"]
 	get_node(extra_arg_0).show()
