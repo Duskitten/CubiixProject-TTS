8b9cee350f0c842db3b84741ce1563b37e59556b
DotMouse <none>
2025-02-03 21:00:44 +0100

    Last change before push and pull request

--- addons/Cubiix_Assets/Mods/TTSAsset/Assets/Objects/Map/AreaLoadingTrigger/area_loading_trigger.gd
+++ addons/Cubiix_Assets/Mods/TTSAsset/Assets/Objects/Map/AreaLoadingTrigger/area_loading_trigger.gd
@@ -5,5 +5,8 @@ func _ready() -> void:
 	body_entered.connect(_on_player_entered_loading_trigger)
 
 
+
 func _on_player_entered_loading_trigger() -> void:
+	
+	# Insert code to load the next map here
 	pass
