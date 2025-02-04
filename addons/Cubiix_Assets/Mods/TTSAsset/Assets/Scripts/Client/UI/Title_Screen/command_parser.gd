cbb612c0094e7f3f80df9851dd4eed53db8b2be0
Duskitty <Theduskitty@gmail.com>
2025-02-04 09:19:14 -0500

    give command parser memory

--- addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/UI/Title_Screen/command_parser.gd
+++ addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/UI/Title_Screen/command_parser.gd
@@ -4,6 +4,10 @@ extends Node
 @onready var Line:LineEdit = get_node("../LineEdit")
 @onready var Core = get_node("/root/Main_Scene/CoreLoader")
 
+var last_command = []
+var command_pos = 0
+var last_input = ""
+
 func _ready() -> void:
 	RTL.text += "Cubiix Project : TTL Version " + Core.Globals.GameVersion
 
@@ -28,7 +32,28 @@ func command_parser(text:String) -> void:
 					Core.SceneData.call_deferred("load_scene",split_normal[1],{},true,split_normal[2])
 				elif split_normal.size() > 1:
 					Core.SceneData.call_deferred("load_scene",split_normal[1],{},true,"")
+				
 			"commands":
 				RTL.text += "\n Current Commands:"
 				RTL.text += "\n commands: This Screen"
 				RTL.text += "\n swap_map : For Map Switching, syntax \"swap_map mapid spawnlocation\""
+	
+	last_command.append(text)
+	command_pos = last_command.size()
+
+func _on_line_edit_gui_input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_UP:
+			if command_pos == last_command.size():
+				last_input = Line.text
+			
+			if !last_command.is_empty() && command_pos > 0:
+				command_pos -= 1
+				Line.text = last_command[command_pos]
+		if event.pressed and event.keycode == KEY_DOWN:
+			if !last_command.is_empty() && command_pos < last_command.size():
+				command_pos += 1
+				if command_pos == last_command.size():
+					Line.text = last_input
+				else:
+					Line.text = last_command[command_pos]
