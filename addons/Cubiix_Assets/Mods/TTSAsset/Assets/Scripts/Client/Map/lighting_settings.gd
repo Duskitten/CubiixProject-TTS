960fd6670ea6c3556006adcd4f90b17c58e87be0
Duskitty <Theduskitty@gmail.com>
2025-02-09 13:59:48 -0500

    Fix shadow system

--- addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/Map/lighting_settings.gd
+++ addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/Map/lighting_settings.gd
@@ -2,9 +2,8 @@ extends Node
 @onready var Core = get_node("/root/Main_Scene/CoreLoader")
 
 func _process(delta: float) -> void:
-	if get_parent().directional_shadow_mode != Core.Globals.Data["Visuals"]["Shadow_Depth"]:
-		get_parent().directional_shadow_mode = clamp(int(Core.Globals.Data["Visuals"]["Shadow_Depth"]),0,3)
-		if Core.Globals.Data["Visuals"]["Shadow_Depth"] == -1:
-			get_parent().shadow_enabled = false
-		else:
-			get_parent().shadow_enabled = true
+	get_parent().directional_shadow_mode = clamp(int(Core.Globals.Data["Visuals"]["Shadow_Depth"]),0,3)
+	if int(Core.Globals.Data["Visuals"]["Shadow_Depth"]) < 0:
+		get_parent().shadow_enabled = false
+	else:
+		get_parent().shadow_enabled = true
