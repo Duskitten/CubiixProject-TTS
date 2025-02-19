d2bfb1aaac77b95c7a86b29c2afbb9908f76ad9f
Duskitty <Theduskitty@gmail.com>
2025-02-18 20:58:45 -0500

    Revert back to 4.3, Added Character Colorsystem

--- addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/UI/Title_Screen/Character_Editor.gd
+++ addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/UI/Title_Screen/Character_Editor.gd
@@ -4,10 +4,29 @@ extends Control
 @onready var Side_A = $Side_A
 @onready var Side_B = $Side_B
 var Root:Node
+
+var Base_Eyes:String  = ""
+var Base_Ears:String  = ""
+var Base_Extras:String  = ""
+var Base_Tails:String  = ""
+var Base_Wings:String  = ""
+var Acc_Head:String  = ""
+var Acc_Face:String  = ""
+var Acc_Chest:String  = ""
+var Acc_Back:String  = ""
+var Acc_L_Hand:String  = ""
+var Acc_R_Hand:String  = ""
+var Acc_L_Hip:String  = ""
+var Acc_R_Hip:String  = ""
+
+var Target_Node:String = ""
+
 func _ready() -> void:
 	Root = find_parent("DragArea")
 	internal_button_check(Side_A)
 	internal_button_check(Side_B)
+	var character = Root.Temp_Character
+	Base_Eyes = character.get("Base_Eyes")
 	
 func internal_button_check(node:Node) -> void:
 	for i in node.get_children():
@@ -30,10 +49,17 @@ func _on_forward_button_pressed() -> void:
 	Anim.play("Out")
 	set_back()
 
-func _on_back_button_pressed() -> void:
+func _on_back_button_pressed(notkeep:bool = true) -> void:
+	var character = Root.Temp_Character
 	Anim.play_backwards("Out")
 	hide_both()
-	
+	if notkeep:
+		character.set(Target_Node,self.get(Target_Node))
+		character.generate_character()
+	else:
+		self.set(Target_Node,character.get(Target_Node))
+		Core.AssetData.Tools.clone_character_with_accessories(character,Core.Persistant_Core.Player.get_node("Hub"))
+		
 func _on_generate_button_pressed(type:String,subtype:Node) -> void:
 	match type:
 		"Item":
@@ -45,6 +71,7 @@ func _on_generate_button_pressed(type:String,subtype:Node) -> void:
 					extended = "Acc_"
 				false:
 					extended = "Base_"
+			Target_Node = extended+str(subtype.name)
 			var template_controller = $"Side_In-Menu/Menu_Slider/ScrollContainer/VBoxContainer/HBoxContainer/Template_Controller"
 			var container = $"Side_In-Menu/Menu_Slider/ScrollContainer/VBoxContainer/HBoxContainer/VBoxContainer"
 			for i in container.get_children():
@@ -70,6 +97,7 @@ func _on_generate_button_pressed(type:String,subtype:Node) -> void:
 		"Palette":
 			$"Side_In-Menu/Menu_Color".show()
 			$"Side_In-Menu/Menu_Slider".hide()
+			$"Side_In-Menu/Menu_Color".setup()
 			
 func set_new_value(WhatVar:String,WhatValue:String) -> void:
 	#print(WhatVar, WhatValue)
--- addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/UI/Title_Screen/Color_Editor.gd
+++ addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/UI/Title_Screen/Color_Editor.gd
@@ -21,11 +21,37 @@ extends VBoxContainer
 @onready var Metallic:HSlider = $Metallic
 @onready var Roughness:HSlider = $Roughness
 
+var OldBaseCol:Color = Color.html("000000")
+var OldEmissCol:Color = Color.html("000000")
+var OldEmissStrVal:float = 0.0
+var OldMetallicVal:float = 0.0
+var OldRoughnessVal:float = 1.0
+var update_mat = false
+
+
+func setup() -> void:
+	update_mat = false
+	var character = find_parent("Offline").Character_Template
+	var Keylist = character.Keylist
+	Base_Hex.text = character.Shader_Color[Keylist["Body"][Target_Material]].to_html(false)
+	Emiss_Hex.text = character.Shader_Emission[Keylist["Body"][Target_Material]].to_html(false)
+	Metallic_Float.text =  str("%0.2f" % character.Shader_Metallic[Keylist["Body"][Target_Material]])
+	Emiss_Float.text =  str("%0.2f" % character.Shader_Emission_Strength[Keylist["Body"][Target_Material]])
+	Roughness_Float.text =  str("%0.2f" % character.Shader_Roughness[Keylist["Body"][Target_Material]])
+	_on_line_edit_text_changed(character.Shader_Color[Keylist["Body"][Target_Material]].to_html(false),"Base_Color")
+	_on_line_edit_text_changed(character.Shader_Emission[Keylist["Body"][Target_Material]].to_html(false),"Emiss_Color")
+	_on_line_edit_text_changed(str("%0.2f" % character.Shader_Metallic[Keylist["Body"][Target_Material]]),"Metallic")
+	_on_line_edit_text_changed(str("%0.2f" % character.Shader_Emission_Strength[Keylist["Body"][Target_Material]]),"Emiss_Str")
+	_on_line_edit_text_changed(str("%0.2f" % character.Shader_Roughness[Keylist["Body"][Target_Material]]),"Roughness")
+	await get_tree().process_frame
+	update_mat = true
+	
 func remove_text_focus() -> void:
 	get_viewport().gui_release_focus()
 
 func _on_line_edit_text_changed(new_text: String, extra_arg_0: String) -> void:
-	find_parent("TextureRect").set_edited()
+	if update_mat:
+		find_parent("TextureRect").set_edited()
 	match extra_arg_0:
 		"Base_Color":
 			if Color.html_is_valid(new_text):
@@ -38,7 +64,8 @@ func _on_line_edit_text_changed(new_text: String, extra_arg_0: String) -> void:
 				Stylebox.texture.gradient.colors[1] = Color.from_hsv(Compiled_Color.h,1.0,1.0)
 				var Stylebox2:StyleBox = Base_Vib.get_theme_stylebox("slider")
 				Stylebox2.texture.gradient.colors[1] = Color.from_hsv(Compiled_Color.h,1.0,1.0)
-				update_materials()
+				if update_mat:
+					update_materials()
 		"Emiss_Color":
 			if Color.html_is_valid(new_text):
 				var Compiled_Color = Color.html(new_text)
@@ -50,60 +77,66 @@ func _on_line_edit_text_changed(new_text: String, extra_arg_0: String) -> void:
 				Stylebox.texture.gradient.colors[1] = Color.from_hsv(Compiled_Color.h,1.0,1.0)
 				var Stylebox2:StyleBox = Emiss_Vib.get_theme_stylebox("slider")
 				Stylebox2.texture.gradient.colors[1] = Color.from_hsv(Compiled_Color.h,1.0,1.0)
-				update_materials()
+				if update_mat:
+					update_materials()
 		"Emiss_Str":
 			Emiss_Str.value = float(new_text) 
-			update_materials()
+			if update_mat:
+				update_materials()
 		"Metallic":
 			Metallic.value = float(new_text) 
-			update_materials()
+			if update_mat:
+				update_materials()
 		"Roughness":
 			Roughness.value = float(new_text) 
-			update_materials()
+			if update_mat:
+				update_materials()
 	#print()
 
 
 func _on_value_changed(value: float, extra_arg_0: String) -> void:
-	find_parent("TextureRect").set_edited()
-	match extra_arg_0:
-		"Base_Color":
-			if !Base_Hex.has_focus():
-				var Compiled_Color:Color = Color.from_hsv(Base_Hue.value,Base_Sat.value,Base_Vib.value)
-				Base_Hex.text = Compiled_Color.to_html(false)
-				Base_Rect.color = Compiled_Color
-				var Stylebox:StyleBox = Base_Sat.get_theme_stylebox("slider")
-				Stylebox.texture.gradient.colors[1] = Color.from_hsv(Base_Hue.value,1.0,1.0)
-				var Stylebox2:StyleBox = Base_Vib.get_theme_stylebox("slider")
-				Stylebox2.texture.gradient.colors[1] = Color.from_hsv(Base_Hue.value,1.0,1.0)
-				update_materials()
-		"Emiss_Color":
-			if !Emiss_Hex.has_focus():
-				var Compiled_Color:Color = Color.from_hsv(Emiss_Hue.value,Emiss_Sat.value,Emiss_Vib.value)
-				Emiss_Hex.text = Compiled_Color.to_html(false)
-				Emiss_Rect.color = Compiled_Color
-				var Stylebox:StyleBox = Emiss_Sat.get_theme_stylebox("slider")
-				Stylebox.texture.gradient.colors[1] = Color.from_hsv(Emiss_Hue.value,1.0,1.0)
-				var Stylebox2:StyleBox = Emiss_Vib.get_theme_stylebox("slider")
-				Stylebox2.texture.gradient.colors[1] = Color.from_hsv(Emiss_Hue.value,1.0,1.0)
-				update_materials()
-		"Emiss_Str":
-			if !Emiss_Float.has_focus():
-				Emiss_Float.text = str("%0.2f" % Emiss_Str.value)
-				update_materials()
-		"Metallic":
-			if !Metallic_Float.has_focus():
-				Metallic_Float.text = str("%0.2f" % Metallic.value)
-				update_materials()
-		"Roughness":
-			if !Roughness_Float.has_focus():
-				Roughness_Float.text = str("%0.2f" % Roughness.value)
-				update_materials()
+	if update_mat:
+		find_parent("TextureRect").set_edited()
+		match extra_arg_0:
+			"Base_Color":
+				if !Base_Hex.has_focus():
+					var Compiled_Color:Color = Color.from_hsv(Base_Hue.value,Base_Sat.value,Base_Vib.value)
+					Base_Hex.text = Compiled_Color.to_html(false)
+					Base_Rect.color = Compiled_Color
+					var Stylebox:StyleBox = Base_Sat.get_theme_stylebox("slider")
+					Stylebox.texture.gradient.colors[1] = Color.from_hsv(Base_Hue.value,1.0,1.0)
+					var Stylebox2:StyleBox = Base_Vib.get_theme_stylebox("slider")
+					Stylebox2.texture.gradient.colors[1] = Color.from_hsv(Base_Hue.value,1.0,1.0)
+					update_materials()
+			"Emiss_Color":
+				if !Emiss_Hex.has_focus():
+					var Compiled_Color:Color = Color.from_hsv(Emiss_Hue.value,Emiss_Sat.value,Emiss_Vib.value)
+					Emiss_Hex.text = Compiled_Color.to_html(false)
+					Emiss_Rect.color = Compiled_Color
+					var Stylebox:StyleBox = Emiss_Sat.get_theme_stylebox("slider")
+					Stylebox.texture.gradient.colors[1] = Color.from_hsv(Emiss_Hue.value,1.0,1.0)
+					var Stylebox2:StyleBox = Emiss_Vib.get_theme_stylebox("slider")
+					Stylebox2.texture.gradient.colors[1] = Color.from_hsv(Emiss_Hue.value,1.0,1.0)
+					update_materials()
+			"Emiss_Str":
+				if !Emiss_Float.has_focus():
+					Emiss_Float.text = str("%0.2f" % Emiss_Str.value)
+					update_materials()
+			"Metallic":
+				if !Metallic_Float.has_focus():
+					Metallic_Float.text = str("%0.2f" % Metallic.value)
+					update_materials()
+			"Roughness":
+				if !Roughness_Float.has_focus():
+					Roughness_Float.text = str("%0.2f" % Roughness.value)
+					update_materials()
 
 func update_materials() -> void:
-	var Keylist = find_parent("Offline").Character_Template.Keylist
-	find_parent("Offline").Character_Template.Shader_Color[Keylist["Body"][Target_Material]] = Color.from_hsv(Base_Hue.value,Base_Sat.value,Base_Vib.value)
-	find_parent("Offline").Character_Template.Shader_Emission[Keylist["Body"][Target_Material]] = Color.from_hsv(Emiss_Hue.value,Emiss_Sat.value,Emiss_Vib.value)
-	find_parent("Offline").Character_Template.Shader_Metallic[Keylist["Body"][Target_Material]] =  Metallic.value
-	find_parent("Offline").Character_Template.Shader_Emission_Strength[Keylist["Body"][Target_Material]] = Emiss_Str.value
-	find_parent("Offline").Character_Template.Shader_Roughness[Keylist["Body"][Target_Material]] = Roughness.value
-	find_parent("Offline").Character_Template.generate_colors()
+	var character = find_parent("Offline").Character_Template
+	var Keylist = character.Keylist
+	character.Shader_Color[Keylist["Body"][Target_Material]] = Color.from_hsv(Base_Hue.value,Base_Sat.value,Base_Vib.value)
+	character.Shader_Emission[Keylist["Body"][Target_Material]] = Color.from_hsv(Emiss_Hue.value,Emiss_Sat.value,Emiss_Vib.value)
+	character.Shader_Metallic[Keylist["Body"][Target_Material]] =  Metallic.value
+	character.Shader_Emission_Strength[Keylist["Body"][Target_Material]] = Emiss_Str.value
+	character.Shader_Roughness[Keylist["Body"][Target_Material]] = Roughness.value
+	character.generate_colors()
