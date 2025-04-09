b66edbf64e4b1fd1271075b0d76f6dee5c856ea4
Duskitten <theduskitty@gmail.com>
2025-04-09 00:27:57 -0400

    Added more UI elements, and bottom bar area

--- addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/UI/Phone/clock.gd
+++ addons/Cubiix_Assets/Mods/TTSAsset/Assets/Scripts/Client/UI/Phone/clock.gd
@@ -1,9 +1,24 @@
 extends Label
 
 var saved_time = [0,0]
-
+var TwelveHR = true
+var AMPM = "AM"
+var Backuptime = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if saved_time != [Time.get_datetime_dict_from_system()["hour"],Time.get_datetime_dict_from_system()["minute"]]:
 		saved_time = [Time.get_datetime_dict_from_system()["hour"],Time.get_datetime_dict_from_system()["minute"]]
-		text = "%02d:%02d" % [saved_time[0], saved_time[1]]
+		if !TwelveHR:
+			text = "%02d:%02d" % [saved_time[0], saved_time[1]]
+		else:
+			if saved_time[0] >= 12:
+				AMPM = "PM"
+			else:
+				AMPM = "AM"
+			
+			if saved_time[0] == 0:
+				Backuptime = 12
+			else:
+				Backuptime = saved_time[0]
+
+			text = "%02d:%02d %s" % [Backuptime, saved_time[1], AMPM]
