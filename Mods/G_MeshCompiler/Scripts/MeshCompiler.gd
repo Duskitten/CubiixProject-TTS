1cac585242617fd8f95524e3e7b0477485437b97
Duskitten <theduskitty@gmail.com>
2025-12-10 19:27:44 -0600

    mesh compiler works!

--- Mods/G_MeshCompiler/Scripts/MeshCompiler.gd
+++ Mods/G_MeshCompiler/Scripts/MeshCompiler.gd
@@ -6,21 +6,18 @@ func compile_mesh(meshfrom:ArrayMesh, meshto:ArrayMesh) -> Dictionary:
 	var replacementmesh = ArrayMesh.new()
 	var replacementaabb = AABB()
 	var replacementsurfacetool = SurfaceTool.new()
-
-	
 	var replacementmeshcommit = [PackedVector3Array(),PackedVector3Array(),PackedFloat32Array(),null,PackedVector2Array(),null,null,null,null,null,PackedInt32Array(),PackedFloat32Array(),PackedInt32Array()]
-
 	
 	replacementsurfacetool.append_from(meshfrom,0,Transform3D())
 	replacementsurfacetool.append_from(meshto,0,Transform3D())
 	
 	var mesha = SurfaceTool.new()
+	mesha.create_from(meshfrom,0)
 	var meshaarray = mesha.commit_to_arrays()
 	var meshb = SurfaceTool.new()
+	meshb.create_from(meshto,0)
 	var meshbarray = meshb.commit_to_arrays()
-	
-	
-	var blendshapes = []
+	var blendshapes: Array[Array] = []
 	var blendshapenamed = {}
 	
 	for i in meshfrom.get_blend_shape_count():
@@ -41,32 +38,51 @@ func compile_mesh(meshfrom:ArrayMesh, meshto:ArrayMesh) -> Dictionary:
 	blendshapes.resize(blendshapenamed.size())
 	
 	for shape in blendshapenamed.keys().size():
-		var shapenames = blendshapenamed[blendshapenamed.keys()[shape]]
-		var compiledblendarray = [PackedVector3Array(),PackedVector3Array(),PackedFloat32Array()]
-		if shapenames["a"] != null:
-			var surfacetoola = SurfaceTool.new()
-			surfacetoola.create_from_blend_shape(meshfrom,0,blendshapenamed.keys()[shape])
-			var blendshapea = surfacetoola.commit_to_arrays()
-			compiledblendarray[0].append_array()
-			compiledblendarray[1].append_array(meshaarray[1])
-			compiledblendarray[2].append_array(meshaarray[2])
-		else:
-			var newpackedvec3 = PackedVector3Array()
-			newpackedvec3.resize(meshaarray[0].size())
-			compiledblendarray[0] = newpackedvec3
-			compiledblendarray[1] = meshaarray[1]
-			compiledblendarray[1] = meshaarray[2]
-			
-		var surfacetoolb = SurfaceTool.new()
 		## Build the blendshape parser here,
 		## we want it so we get the original blendshape, then parse between the two to see if they share said ID
 		## Then we want to splice in the different blendshapes, we can compare with the shapenames for A, B, or Both
 		## if A or B we apply base first (A + Static B) or (Static A + B)
 		## if both, we apply as (A + B)
 		## then we commit to blendshapes variable
-		blendshapes.append(compiledblendarray)
+		var shapenames = blendshapenamed[blendshapenamed.keys()[shape]]
+		var compiledblendarray = [PackedVector3Array(),PackedVector3Array(),PackedFloat32Array()]
+		if shapenames["a"] != null:
+			var surfacetoola = SurfaceTool.new()
+			surfacetoola.create_from_blend_shape(meshfrom,0,blendshapenamed.keys()[shape])
+			var blendshapea = surfacetoola.commit_to_arrays()
+			for x in blendshapea[0].size():
+				blendshapea[0][x] -=   meshaarray[0][x]
+
+			compiledblendarray[0].append_array(blendshapea[0])
+			compiledblendarray[1].append_array(meshaarray[1])
+			compiledblendarray[2].append_array(meshaarray[2])
+		else:
+			var newpackedvec3 = PackedVector3Array()
+			newpackedvec3.resize(meshaarray[0].size())
+			compiledblendarray[0].append_array(newpackedvec3)
+			compiledblendarray[1].append_array(meshaarray[1])
+			compiledblendarray[2].append_array( meshaarray[2])
+			
+		if shapenames["b"] != null:
+			var surfacetoolb = SurfaceTool.new()
+			surfacetoolb.create_from_blend_shape(meshto,0,blendshapenamed.keys()[shape])
+			var blendshapeb = surfacetoolb.commit_to_arrays()
+			
+			for x in blendshapeb[0].size():
+				blendshapeb[0][x] -=   meshbarray[0][x]
+			
+			compiledblendarray[0].append_array(blendshapeb[0])
+			compiledblendarray[1].append_array(meshbarray[1])
+			compiledblendarray[2].append_array(meshbarray[2])
+		else:
+			var newpackedvec3 = PackedVector3Array()
+			newpackedvec3.resize(meshbarray[0].size())
+			compiledblendarray[0].append_array(newpackedvec3)
+			compiledblendarray[1].append_array(meshbarray[1])
+			compiledblendarray[2].append_array( meshbarray[2])
 		
-		
+		blendshapes[shape] = compiledblendarray
+		print(compiledblendarray[0].size())
 	replacementmeshcommit = replacementsurfacetool.commit_to_arrays()
 	
 	replacementmesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES,replacementmeshcommit,blendshapes)
