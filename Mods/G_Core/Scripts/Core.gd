fab3dd756c377d5d857233c38e824421f268df50
Duskitten <theduskitty@gmail.com>
2025-12-23 18:11:19 -0600

    Finished up base map mesh

--- Mods/G_Core/Scripts/Core.gd
+++ Mods/G_Core/Scripts/Core.gd
@@ -11,8 +11,8 @@ func _ready() -> void:
 	var earmesh = load("res://Mods/G_CubiixCore/Meshes/Cubiix_Base_MK4_Ears_1.gltf").instantiate()
 	var tailmesh = load("res://Mods/G_CubiixCore/Meshes/Cubiix_Base_MK4_Tail_1.gltf").instantiate()
 	var wingmesh = load("res://Mods/G_CubiixCore/Meshes/Cubiix_Base_MK4_Wings_1.gltf").instantiate()
-	var newmeshdict:Dictionary = gmeshcompiler.compile_mesh($Node3D/Cubiix_Base_MK4_Core/Armature/Skeleton3D/Body.mesh,earmesh.get_node("Armature/Skeleton3D/Ears_1").mesh, false)
-	var oldmat = $Node3D/Cubiix_Base_MK4_Core/Armature/Skeleton3D/Body.mesh.surface_get_material(0)
+	var newmeshdict:Dictionary = gmeshcompiler.compile_mesh($Core/Characters/Cubiix/Model/Armature/Skeleton3D/Body.mesh,earmesh.get_node("Armature/Skeleton3D/Ears_1").mesh, false)
+	var oldmat = $Core/Characters/Cubiix/Model/Armature/Skeleton3D/Body.mesh.surface_get_material(0)
 	RenderingServer.mesh_set_custom_aabb(newmeshdict["mesh"].get_rid(), newmeshdict["aabb"])
 	
 	newmeshdict = gmeshcompiler.compile_mesh(newmeshdict["mesh"],wingmesh.get_node("Armature/Skeleton3D/Wings_1").mesh, true)
@@ -24,8 +24,8 @@ func _ready() -> void:
 	
 	
 	
-	$Node3D/Cubiix_Base_MK4_Core/Armature/Skeleton3D/Body.mesh = newmeshdict["mesh"]
-	$Node3D/Cubiix_Base_MK4_Core/Armature/Skeleton3D/Body.set_blend_shape_value(0,1.0)
+	$Core/Characters/Cubiix/Model/Armature/Skeleton3D/Body.mesh = newmeshdict["mesh"]
+	$Core/Characters/Cubiix/Model/Armature/Skeleton3D/Body.set_blend_shape_value(0,1.0)
 
 func reload_mods_and_assets():
 	gassetmanager.reset_asset_table()
